307958de844765fdd37ef3c7cdf9f5851d0e9c85
Ole Kleinschmidt <ok@datenreisende.org>
2023-07-15 19:29:33 +0000

    feat(ldap): show groups of a user, show members of a group

--- src/pyadm/ldapcli/click_commands.py
+++ src/pyadm/ldapcli/click_commands.py
@@ -14,6 +14,7 @@ defaults = {
 
 click_options = {}
 
+# ldap wrapper function(s)
 def ldap_search(click_options, search_filter, attributes=[]):
     try: 
         ldap_connection = ldap.initialize(click_options["server"])
@@ -24,7 +25,8 @@ def ldap_search(click_options, search_filter, attributes=[]):
         return result
     except ldap.LDAPError as e:
         raise click.ClickException(f"LDAP search failed: {e}")
-    
+
+# define click commands    
 @click.group("ldap", help="Query LDAP/Active Directory")
 @click.option('--server', '-s', help='LDAP Server')
 @click.option('--base_dn', '-b', help='base_dn')
@@ -41,13 +43,14 @@ def ldapcli(server, base_dn, username, password):
         click_options["base_dn"] = base_dn or defaults["base_dn"]
         click_options["username"] = username or defaults["username"]
         click_options["password"] = password
-  
+ 
+# show information about a user  
 @ldapcli.command("user", help="Show information about [USER]")
 @click.argument('username')
 @click.option('--all', '-a', is_flag=True, default=None, help="Show all attributes")
 @click.option('--json', '-j', 'json_output', is_flag=True, default=None, help="Output as JSON")
 def user(username, json_output, all):
-    search_filter = f"(uid={username})"
+    search_filter = f"(cn={username})"
     try:
         if all:
             result = ldap_search(click_options, search_filter)
@@ -57,7 +60,9 @@ def user(username, json_output, all):
 
         if result:
             dn, user_info = result[0]
+            # decode bytecode values to utf-8, otherwise every value will have a b' in front.
             user_info = {attr: [value.decode('utf-8') for value in values] for attr, values in user_info.items()}
+            
             if json_output:
                 print(json.dumps(user_info, default=str, indent=4))
             else:
@@ -68,6 +73,88 @@ def user(username, json_output, all):
                
                 for attr, values in user_info.items():
                     if attr == 'memberOf':
+                        print(f"{attr}:")
+                        for group in values:
+                            print(f"  {group}")
+                    elif attr == 'objectClass':
+                        print(f"{attr}:")
+                        for objectclass in values:
+                            print(f"  {objectclass}")
+                    else:
+                        print(f"{attr}: {', '.join(values)}")
+        else:
+            raise click.ClickException(f"No user found with UID '{username}'.")
+    except click.ClickException as e:
+        raise e
+    except Exception as e:
+        raise click.ClickException(f"An error occurred: {e}")
+
+# show groups a user belongs to
+@ldapcli.command("groups", help="Show groups for [USER]")
+@click.argument('user_cn')
+@click.option('--all', '-a', is_flag=True, default=None, help="Show all attributes")
+@click.option('--json', '-j', 'json_output', is_flag=True, default=None, help="Output as JSON")
+def groups(user_cn, json_output, all):
+    search_filter = f"(cn={user_cn})"
+    try:
+        if all:
+            result = ldap_search(click_options, search_filter)
+        else:
+            attributes = ['memberOf']
+            result = ldap_search(click_options, search_filter, attributes)
+
+        if result:
+            dn, group_info = result[0]
+            # decode bytecode values to utf-8, otherwise every value will have a b' in front.            
+            group_info = {attr: [value.decode('utf-8') for value in values] for attr, values in group_info.items()}
+            if json_output:
+                print(json.dumps(group_info, default=str, indent=4))
+            else:
+                if 'memberOf' in group_info:
+                    group_info['memberOf'] = [f"- {group}" for group in group_info['memberOf']]
+               
+                for attr, values in group_info.items():
+                    if attr == 'memberOf':
+                        print(f"{attr}:")
+                        for group in values:
+                            print(f"  {group}")
+                    else:
+                        print(f"{attr}: {', '.join(values)}")
+        else:
+            raise click.ClickException(f"No user found with UID '{username}'.")
+    except click.ClickException as e:
+        raise e
+    except Exception as e:
+        raise click.ClickException(f"An error occurred: {e}")
+
+# show members of a group
+@ldapcli.command("members", help="Show members of [GROUP]")
+@click.argument('group_cn')
+@click.option('--all', '-a', is_flag=True, default=None, help="Show all attributes")
+@click.option('--json', '-j', 'json_output', is_flag=True, default=None, help="Output as JSON")
+def members(group_cn, json_output, all):
+    search_filter = f"(cn={group_cn})"
+    try:
+        if all:
+            result = ldap_search(click_options, search_filter)
+        else:
+            attributes = ['cn', 'description', 'member']
+            result = ldap_search(click_options, search_filter, attributes)
+
+        if result:
+            dn, group_info = result[0]
+            # decode bytecode values to utf-8, otherwise every value will have a b' in front.            
+            group_info = {attr: [value.decode('utf-8') for value in values] for attr, values in group_info.items()}
+            if json_output:
+                print(json.dumps(group_info, default=str, indent=4))
+            else:
+                if 'member' in group_info:
+                    group_info['member'] = [f"- {group}" for group in group_info['member']]
+                if 'objectClass' in group_info:
+                    group_info['objectClass'] = [f"- {objectclass}" for objectclass in group_info['objectClass']]
+               
+                for attr, values in group_info.items():
+                    if attr == 'member':
                         print(f"{attr}:")
                         for group in values:
                             print(f"  {group}")
